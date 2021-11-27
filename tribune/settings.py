599c86b0216bbc4de66e83ea3df9720f7cdf99af
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-27 17:49:30 +0300

    Modification of settings

--- tribune/settings.py
+++ tribune/settings.py
@@ -104,15 +104,6 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tribune',
-#         'USER': 'moringa',
-#     'PASSWORD':'kipngeno',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
